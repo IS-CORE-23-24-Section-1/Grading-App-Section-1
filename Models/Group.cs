2c4f6a28b06031dfa37fd992a5e44a5a50a4b876
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-03-27 10:11:33 -0600

    add group model

--- Models/Group.cs
+++ Models/Group.cs
@@ -1,7 +1,21 @@
-﻿namespace Grading_App_Section_1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grading_App_Section_1.Models
 {
     public class Group
     {
+        [Key]
+        public int group_id { get; set; }
 
+        [Required]
+        public int section_number { get; set; }
+
+        [Required]
+        public int group_number { get; set; }
+
+        public string submission_link { get; set; }
+
+        [Required] 
+        public int group_modifier { get; set; } = 0;
     }
 }
